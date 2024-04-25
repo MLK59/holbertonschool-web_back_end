4925519caec8255927da9a94de464ad7d05b24cc
MLK59 <abdelmalekmhamed@gmail.com>
2024-04-26 00:33:48 +0200

    change the order of the code

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -15,5 +15,5 @@ async def async_generator() -> Generator[float, None, None]:
     between 0 and 10
     """
     for _ in range(10):
-        await asyncio.sleep(1)
         yield random.unfiform(0, 10)
+        await asyncio.sleep(1)
