c03d9ea3a92d5b345ff9d0bb368abdb7e3603278
MLK59 <abdelmalekmhamed@gmail.com>
2024-05-28 15:09:43 +0200

    Eslint : update, remove unnecessary whitespace

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,11 @@
 const students = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
   
   function getListStudents() {
     return students;
   }
-  
+
   export default getListStudents;
